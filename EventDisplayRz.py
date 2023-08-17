40c478407ebb4f8061e4350253fed6d0b6ef3090
AdamVendrasco <avendras@vols.utk.edu>
2023-08-17 14:01:29 -0500

    Small update

--- EventDisplayRz.py
+++ EventDisplayRz.py
@@ -28,8 +28,7 @@ for ievt, event in enumerate(reader):
   '''
   #within file for only MC particles   
   for mcp in mcpCollection:
-      charge = mcp.getCharge()
-      status = mcp.getGeneratorStatus()
+      >cd(1);
       if fabs(charge) > 0:
           if fabs(mcp.getPDG()) == 11:
               vx = mcp.getVertex()
@@ -46,108 +45,121 @@ for ievt, event in enumerate(reader):
   '''
   #gets relevant pfos
   for pfo in pfoCollection:
-
-
     if fabs(pfo.getType()) == 11:
-      dp3 = pfo.getMomentum()
+      dp3=pfo.getMomentum()
       tlv_pfo = TLorentzVector()
-      tlv_pfo.SetPxPyPzE(dp3[0], dp3[1], dp3[2], pfo.getEnergy())
-      
-      phipos= tlv_pfo.Phi()
-      etapos=tlv_pfo.Eta()
-      
+      tlv_pfo.SetPxPyPzE(dp3[0], dp3[1], dp3[2], pfo.getEnergy()) 
+      Rpos=tlv_pfo.Rho()
+      Zpos=tlv_pfo.Z()    
+       
       ele=TGraph()
-      ele.SetPoint(0, phipos, etapos)
+      ele.SetPoint(0, Zpos, Rpos)
       ele.SetMarkerStyle(24)
       ele.SetMarkerColor(kBlue)  
-      ele.SetMarkerSize(log(pfo.getEnergy()))
+      ele.SetMarkerSize(fabs(log(pfo.getEnergy())))     
+      arrow=TArrow(0,0,Zpos,Rpos)
+      arrow.SetArrowSize(log(tlv_pfo.Pt())/300)
+      arrow.SetLineWidth(2)
       mg.Add(ele)
-    
-    if fabs(pfo.getType())==2112:    
-      dp3 = pfo.getMomentum()
-      tlv_pfo = TLorentzVector()
-      tlv_pfo.SetPxPyPzE(dp3[0], dp3[1], dp3[2], pfo.getEnergy())
-      phipos=tlv_pfo.Phi()
-      etapos=tlv_pfo.Eta()
-      neut = TGraph()
-      neut.SetPoint(0, phipos, etapos)
-      neut.SetMarkerColor(kRed)
-      neut.SetMarkerStyle(24)
-      neut.SetMarkerSize(log(pfo.getEnergy()))
-      #mg.Add(neut)
-        
-    if fabs(pfo.getType())==22:    
-      dp3 = pfo.getMomentum()
-      tlv_pfo = TLorentzVector()
-      tlv_pfo.SetPxPyPzE(dp3[0], dp3[1], dp3[2], pfo.getEnergy())
-      phipos=tlv_pfo.Phi()
-      etapos=tlv_pfo.Eta()
-      pho=TGraph()
-      pho.SetPoint(0, phipos, etapos)
-      pho.SetMarkerColor(kGreen)
-      pho.SetMarkerStyle(24)
-      pho.SetMarkerSize(log(pfo.getEnergy()))                        
-      #mg.Add(pho)
+      print(tlv_pfo.Pt()) 
+   
+   # if fabs(pfo.getType())==2112:    
+   #   dp3 = pfo.getMomentum()
+   #   tlv_pfo = TLorentzVector()
+   #   tlv_pfo.SetPxPyPzE(dp3[0], dp3[1], dp3[2], pfo.getEnergy())
+   #   phipos=tlv_pfo.Phi()
+   #   etapos=tlv_pfo.Eta()
+   #   neut = TGraph()
+   #   neut.SetPoint(0, phipos, etapos)
+   #   neut.SetMarkerColor(kRed)
+   #   neut.SetMarkerStyle(24)
+   #   neut.SetMarkerSize(log(pfo.getEnergy()))
+   #   #mg.Add(neut)
+   #     
+   # if fabs(pfo.getType())==22:    
+   #   dp3 = pfo.getMomentum()
+   #   tlv_pfo = TLorentzVector()
+   #   tlv_pfo.SetPxPyPzE(dp3[0], dp3[1], dp3[2], pfo.getEnergy())
+   #   
+   #   pho=TGraph()
+   #   pho.SetPoint(0, Zpos, Rpos)
+   #   pho.SetMarkerStyle(24)
+   #   pho.SetMarkerColor(kGreen)  
+   #   pho.SetMarkerSize(fabs(log(pfo.getEnergy())))     
+   #   arrow=TArrow(0,0,Zpos,Rpos)
+   #   arrow.SetArrowSize(log(tlv_pfo.Pt())/300)
+   #   arrow.SetLineWidth(2)     
+   #   mg.Add(pho)
   
   for mcp in mcpCollection:
     if fabs(mcp.getPDG()) == 11:
+      
       dp3 = mcp.getVertex()
       tlv_mcp = TLorentzVector()
-      tlv_mcp.SetXYZT(dp3[0], dp3[1], dp3[2], dp3[4])
-      
-      Rhopos= tlv_mcp.Rho()
-      etapos=tlv_mcp.Eta()
-  
+      tlv_mcp.SetXYZT(dp3[0], dp3[1], dp3[2], dp3[3])
+      Rpos=tlv_mcp.Rho()
+      Zpos=tlv_mcp.Z()
       mcp_ele=TGraph()
-      mcp_ele.SetPoint(0, Rhopos,Rhopos*math.cos(etapos))
+      mcp_ele.SetPoint(0,Zpos,Rpos)
       mcp_ele.SetMarkerStyle(2)
       mcp_ele.SetMarkerColor(kMagenta)  
       #mcp_ele.SetMarkerSize(log(mcp.getEnergy()))
       mg.Add(mcp_ele)
+     
+     # print("getVertex index 0: "+str(dp3[0])) 
+     # print("getVertex index 1: "+str(dp3[1]))
+     # print("getVertex index 2: "+str(dp3[2]))
+     # print("getVertex index 3: "+str(dp3[3]))
+     # print("getVertex index 4: "+str(dp3[4]))
+     # print("getVertex index 5: "+str(dp3[5]))
+     # print("getVertex index 6: "+str(dp3[6]))
+     # print("getVertex index 7: "+str(dp3[7]))
+     # print("---------")
   
   #draws multigraph
   c2=TCanvas("c%i"%ievt,"c%i"%ievt,700,500)
-  mg.Draw("APL")
-  mg.SetTitle("PFO Event Display;R [mm];Z [mm]")
-  mg.GetXaxis().SetLimits(-500,4000)
-  mg.GetYaxis().SetRangeUser(-4000,4000)
+  
+  mg.Draw("AP")
+  arrow.Draw() 
+  mg.SetTitle("PFO Event Display;Z [mm];R [mm]")
+  mg.GetXaxis().SetLimits(-2500,2500)
+  mg.GetYaxis().SetRangeUser(-500,3500)
 
   #Manually making legend
-#  TL1=TLatex(2,3.5,"Electron") 
-#  TL2=TLatex(2,3.0,"Neutron")
-#  TL3=TLatex(2,2.5,"Photon")
-#  TL4=TLatex(1.2,2.0,"mcp Electron")
+  TL1=TLatex(1000,3200,"Electron") 
+  #TL2=TLatex(2,3.0,"Neutron")
+  #TL3=TLatex(2,2.5,"Photon")
+  TL4=TLatex(1000,3000,"mcp Electron")
 
-#  TL1.SetTextFont(42)
-#  TL2.SetTextFont(42)
-#  TL3.SetTextFont(42)
-#  TL4.SetTextFont(42)
+  TL1.SetTextFont(42)
+  #TL2.SetTextFont(42)
+  #TL3.SetTextFont(42)
+  TL4.SetTextFont(42)
 
-#  Ecircle=TEllipse(3.4,3.6,0.15,0.18)
-#  Ncircle=TEllipse(3.4,3.1,0.15,0.18)
-#  Pcircle=TEllipse(3.4,2.6,0.15,0.18)
-#  MCPele=TEllipse(3.4,2.1,0.15,0.18)
+  Ecircle=TEllipse(2000,3300,100,100)
+  #Ncircle=TEllipse(3.4,3.1,0.15,0.18)
+  #Pcircle=TEllipse(3.4,2.6,0.15,0.18)
+  MCPele=TMarker(2300,3075,2)
 
-#  Ecircle.SetLineColor(kBlue)
-#  Ncircle.SetLineColor(kRed)
-#  Pcircle.SetLineColor(kGreen)
-#  MCPele.SetLineColor(kMagenta)
+  Ecircle.SetLineColor(kBlue)
+  #Ncircle.SetLineColor(kRed)
+  #Pcircle.SetLineColor(kGreen)
+  MCPele.SetMarkerColor(kMagenta)
+  MCPele.SetMarkerSize(2)
+  Ecircle.SetLineWidth(1)
+  #Ncircle.SetLineWidth(1)
+  #Pcircle.SetLineWidth(1)
+  #MCPele.SetMarkerWidth(1)
 
-#  Ecircle.SetLineWidth(1)
-#  Ncircle.SetLineWidth(1)
-#  Pcircle.SetLineWidth(1)
-#  MCPele.SetLineWidth(1)
+  Ecircle.Draw()
+  #Ncircle.Draw()
+  #Pcircle.Draw()
+  MCPele.Draw()
 
-#  Ecircle.Draw()
-#  Ncircle.Draw()
-#  Pcircle.Draw()
-#  MCPele.Draw()
-
-#  TL1.Draw()
-#  TL2.Draw()
-#  TL3.Draw()
-#  TL4.Draw()
+  TL1.Draw()
+  #TL2.Draw()
+  #TL3.Draw()
+  TL4.Draw()
 
   reader.close()
   c2.SaveAs("RZevent%i.png"%ievt)
-
